38c346ad27ed02db15cbc4bd40087f6820e33054
entereenter <s101019105@gmail.com>
2024-11-19 15:00:43 +0300

    1.2

---
+++ shell.py
@@ -0,0 +1,53 @@
+class Shell:
+    def __init__(self, user, vfs):
+        self.user = user
+        self.vfs = vfs
+
+    def execute_command(self, command):
+        parts = command.strip().split()
+
+
+        cmd = parts[0]
+        args = parts[1:]
+
+        if cmd == "ls":
+            return self.list_files(args)
+        elif cmd == "cd":
+            return self.change_directory(args)
+        elif cmd == "wc":
+            return self.word_count(args)
+        elif cmd == "exit":
+            return "Exiting..."
+        elif cmd == "uname":
+            return self.uname()
+        else:
+            return f"Command '{cmd}' not found."
+
+    def list_files(self, args):
+        try:
+            path = args[0] if args else None
+            files = self.vfs.list_files(path)
+            return "\n".join(files)
+        except Exception as e:
+            return str(e)
+
+    def change_directory(self, args):
+        if not args:
+            return "Usage: cd <path>"
+        try:
+            new_path = self.vfs.change_directory(args[0])
+            return f"Changed directory to {new_path}"
+        except Exception as e:
+            return str(e)
+
+    def word_count(self, args):
+        if not args:
+            return "Usage: wc <file_name>"
+        try:
+            lines, words, chars = self.vfs.wc(args[0])
+            return f"Lines: {lines}, Words: {words}, Characters: {chars}"
+        except Exception as e:
+            return str(e)
+
+    def uname(self):
+        return "Windows"
